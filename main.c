460d91679509fa9ab59f0a8b2e40e9c4ce203ba8
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2024-09-14 19:20:31 +0600

    Upcounter ss not working

--- main.c
+++ main.c
@@ -16,15 +16,23 @@ int main(void){
   DDRD  |=  (1<<5)|(1<<6);
   PORTD &=~ ((1<<5)|(1<<6));
   _delay_ms(500);
-  Timebase_DownCounter_SS_Set_Securely(0, 10000);
-  Timebase_DownCounter_SS_Set_Securely(1, 8000);
-  Timebase_DownCounter_SS_Set_Securely(2, 6000);
-  Timebase_DownCounter_SS_Set_Securely(3, 4000);
+  Timebase_UpCounter_SS_Set_Securely(0, 20000);
+  Timebase_UpCounter_Set_Securely(0, 18);
+  Timebase_DownCounter_SS_Set_Securely(0, 16000);
+  Timebase_DownCounter_Set_Securely(0, 14);
   
   while(1){
     
-	Timebase_Timer_Await_SubSeconds(100);
-	UART_Transmit_Text("Timebase\r\n");
+	Timebase_Timer_Await_SubSeconds(50);
+	
+	UART_Transmit_Number(Timebase_UpCounter_SS_Get_Value(0));
+	UART_Transmit_Text(",");
+	UART_Transmit_Number(Timebase_UpCounter_Get_Value(0));
+	UART_Transmit_Text(",");
+	UART_Transmit_Number(Timebase_DownCounter_SS_Get_Value(0));
+	UART_Transmit_Text(",");
+	UART_Transmit_Number(Timebase_DownCounter_Get_Value(0));
+	UART_Transmit_Text("\r\n");
     /*UART_Transmit_Text("Timebase ");
 	UART_Transmit_Number(Timebase_Timer_Get_Seconds());
 	UART_Transmit_Text(" ");
